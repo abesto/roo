5cc07178635310d8b267385c9528d14883545ff8
Zoltan Nagy <abesto0@gmail.com>
2021-09-02 22:48:45 +0100

    Rhai PoC

--- integration_tests/test_sanity.py
+++ integration_tests/test_sanity.py
@@ -2,12 +2,17 @@
 from .conftest import Connect
 
 
-def test_echo(connect: Connect):
+def test_simple_rhai(connect: Connect):
     c1 = connect()
     c2 = connect()
 
-    c1.send("c1 says hi")
-    c1.expect_exact("c1 says hi")
+    c1.send(";40 + 2")
+    c1.expect_lines_exact("42")
 
-    c2.send("c2 says hi")
-    c2.expect_exact("c2 says hi")
+    c1.send(";let x = 20")
+    c1.send(";x")
+    c1.expect_lines_exact("20")
+
+    # Verify variables don't leak between connections
+    c2.send(";x")
+    c2.expect_exact('ErrorVariableNotFound("x", 1:1)')
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,77 @@
-use tokio::io::AsyncWriteExt;
-use tokio::io::AsyncBufReadExt;
-use tokio::io::BufReader;
-use tokio::net::TcpListener;
-use std::error::Error;
-use tokio;
+use anyhow::Result;
+use tokio::{self, io::{AsyncBufReadExt, AsyncWriteExt, BufReader}, net::{TcpListener, TcpStream, tcp::{OwnedReadHalf, OwnedWriteHalf}}};
+use rhai::{Engine, Dynamic, Scope};
+use async_channel::{Sender, Receiver};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<()> {
+    let listener = listen().await?;
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+        handle_connection(socket);
+    }
+}
+
+fn handle_connection(socket: TcpStream) {
+    tokio::spawn(async move {
+        let (read, write) = socket.into_split();
+
+        let engine = Engine::new();
+        let (line_tx, line_rx) = async_channel::unbounded::<String>();
+        spawn_read_task(read, line_tx);
+        spawn_processing_task(engine, write, line_rx);
+    });
+}
+
+async fn listen() -> Result<TcpListener> {
     println!("Server started");
     let addr = "127.0.0.1:8888";
     let listener = TcpListener::bind(&addr).await?;
     println!("Listening on: {}", addr);
+    Ok(listener)
+}
 
-    loop {
-        let (socket, _) = listener.accept().await?;
-        let (read, mut write) = socket.into_split();
-        let mut lines = BufReader::new(read).lines();
-        tokio::spawn(async move {
-            while let Some(line) = lines.next_line().await.unwrap() {
-                let response = line + "\n";
-                write.write_all(response.as_bytes()).await.unwrap();
+fn spawn_read_task(read: OwnedReadHalf, line_tx: Sender<String>) {
+    let mut lines = BufReader::new(read).lines();
+    tokio::spawn(async move {
+        while let Some(line) = lines.next_line().await.unwrap() {
+            line_tx.send(line).await.unwrap();
+        }
+    });
+}
+
+fn spawn_processing_task(engine: Engine, mut write: OwnedWriteHalf, line_rx: Receiver<String>) {
+    tokio::spawn(async move {
+        let mut scope = Scope::new();
+        loop {
+            let line = match line_rx.recv().await {
+                Ok(l) => l,
+                Err(e) => {
+                    // TODO err logging
+                    println!("{}", e);
+                    break;
+                }
+            };
+
+            println!("< {}", line);
+            if let Some(stripped) = line.strip_prefix(';') {
+                let result = engine.eval_with_scope::<Dynamic>(&mut scope, &stripped);
+                let maybe_msg = match result {
+                    Ok(x) => {
+                        if !x.is::<()>() {
+                            Some(format!("{:?}\n", x))
+                        } else {
+                            None
+                        }
+                    }
+                    Err(e) => Some(format!("{:?}\n", e))
+                };
+
+                if let Some(msg) = maybe_msg {
+                    write.write_all(msg.as_bytes()).await.unwrap();
+                }
             }
-        });
-    }
+        } 
+    });
 }
